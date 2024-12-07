7891fbba768305ad3c161663d4411ad94d63776c
Tyler K <tj29knap@siena.edu>
2024-12-07 12:33:33 -0500

    readded print statement for total miles

--- DijkstraJava/Dijkstra.java
+++ DijkstraJava/Dijkstra.java
@@ -191,6 +191,9 @@ public class Dijkstra {
             System.out.println(g.vertices[hop.dest].label + " for " + df.format(sameRouteLength) + " miles along " + currentRoute);
             totalLength += sameRouteLength;
             hopNum--;
+            if (hopNum < 0) {
+                System.out.println("Total length: " + df.format(totalLength) + " miles");
+            }
         }
 
 		//end timer and print out the time taken to compute
